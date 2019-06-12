e6b0ce07131d38710fbaded1d11c87912f1792d1
f-hawkkai <1394290956@qq.com>
2019-06-12 17:16:24 +0800

    测试文件

---
+++ src/components/header.vue
@@ -0,0 +1,331 @@
+<template>
+    <div class="head">
+         <div class="tou">
+      <ul class="tu">
+        <li class="oli ali">
+          <img src="../assets/img/tou.jpg" width="220" height="50px">
+        </li>
+        <li class="oli">
+          <img src="../assets/img/tou1.jpg">
+        </li>
+        <li class="ol">
+          <h3 class="zi lg">成长守护平台</h3>
+          <h3 class="zi">腾讯游戏排行榜</h3>
+        </li>
+      </ul>
+    </div>
+    <div class="header">
+      <!--一级导航-->
+      <div class="header-inner">
+        <div class="log">
+          <img src="../assets/img/alogo.png">
+        </div>
+        <ul class="let">
+          <li>
+            <a href="#">
+              官网首页
+              <em>home</em>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              游戏资料
+              <em>data</em>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              内容中心
+              <em>contents</em>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              赛事中心
+              <em>match</em>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              百态王者
+              <em>culture</em>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              社区互动
+              <em>community</em>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              玩家支持
+              <em>player</em>
+            </a>
+          </li>
+        </ul>
+        <div class="login">
+          <span>
+            <a href="#">
+              <img src="../assets/img/login.png">
+            </a>
+          </span>
+          <div class="log_in">
+            <a href="#">欢迎登陆</a>
+            <p>登录后查看游戏战绩</p>
+          </div>
+        </div>
+      </div>
+      <!-- 二级导航 -->
+      <div class="header_two">
+        <ul>
+          <li>&nbsp;</li>
+          <li>
+            <a href="#">版本介绍</a>
+            <a href="#">游戏介绍</a>
+            <a href="#">英雄资料</a>
+            <a href="#">爆料站</a>
+            <a href="#">故事站</a>
+            <a href="#">游戏壁纸</a>
+          </li>
+          <li>
+            <a href="#">攻略中心</a>
+            <a href="#">英雄攻略</a>
+            <a href="#">视频中心</a>
+            <a href="#">王者视角</a>
+            <a href="#">作者入驻</a>
+            <a href="#">媒体入驻</a>
+          </li>
+          <li>
+            <a href="#">KPL</a>
+            <a href="#">KPKPL</a>
+            <a href="#">冠军杯</a>
+            <a href="#">高校赛</a>
+            <a href="#">城市赛</a>
+            <a href="#">全民赛</a>
+            <a href="#">赛事数据</a>
+          </li>
+          <li>
+            <a href="#">荣耀·传承</a>
+            <a href="#">王者文化站</a>
+            <a href="#">王者音乐所</a>
+            <a href="#">万千世界</a>
+            <a href="#">星光殿堂</a>
+            <a href="#">WiFi特权</a>
+          </li>
+          <li>
+            <a href="#">王者营地</a>
+            <a href="#">手Q部落</a>
+            <a href="#">微信圈子</a>
+            <a href="#">官方微博</a>
+            <a href="#">微信公众号</a>
+            <a href="#">手Q订阅号</a>
+            <a href="#">同仁社区</a>
+          </li>
+          <li>
+            <a href="#">成长守护平台</a>
+            <a href="#">健康系统</a>
+            <a href="#">狄仁杰护卫站</a>
+            <a href="#">客服专区</a>
+            <a href="#">礼包兑换</a>
+            <a href="#">自助服务</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+em {
+  font-style: normal;
+}
+body {
+  padding-top: 42px;
+  overflow-x: hidden;
+}
+a {
+  text-decoration: none;
+  color: #c9c9dd;
+}
+.tou {
+  width: 100%;
+  border-width: 1px 0;
+  position: absolute;
+  height: 42px;
+  left: 0;
+  top: 0 !important;
+  border: 1px solid #c9c9c9;
+}
+.tou .tu {
+  width: 950px;
+  margin: 0 auto;
+}
+.oli {
+  float: left;
+}
+.ali {
+  width: 205px;
+  padding: 0 15px 0 0;
+  background-position: 0 -40px;
+  position: relative;
+  z-index: 10001;
+}
+.ol {
+  float: right;
+}
+.zi {
+  float: left;
+  width: 118px;
+  font-size: 12px;
+  font-weight: 400;
+  cursor: pointer;
+  text-indent: 2rem;
+  line-height: 40px;
+}
+.lg {
+  background: url(../assets/img/logo.png) 12px no-repeat;
+}
+.header {
+  width: 100%;
+  position: absolute;
+  top: 42px;
+  left: 0;
+  height: 85px;
+  background: rgba(0, 0, 0, 0.8);
+}
+.header:hover .header_two {
+  height: 240px;
+}
+.log {
+  float: left;
+}
+.log img {
+  position: absolute;
+  top: 16px;
+  left: 0;
+}
+.header-inner {
+  width: 1200px;
+  height: 84px;
+  position: relative;
+  margin: 0 auto;
+}
+.let {
+  width: 790px;
+  float: left;
+  padding-left: 230px;
+}
+.let li {
+  width: 110px;
+  float: left;
+  height: 84px;
+  text-align: center;
+  box-sizing: border-box;
+}
+.let li:hover {
+  border-bottom: 3px solid #e4b653;
+}
+.let li:hover a {
+  color: #e4b653;
+}
+.let li:hover a em {
+  color: #e4b653;
+}
+.let li a {
+  display: block;
+  width: 110px;
+  height: 64px;
+  line-height: 30px;
+  color: #c9c9dd;
+  font-size: 18px;
+  padding: 20px 0 0;
+}
+.let li a em {
+  display: block;
+  padding-top: 5px;
+  font-size: 12px;
+  line-height: 16px;
+  color: #858792;
+  text-transform: uppercase;
+}
+.login {
+  width: 182px;
+  position: absolute;
+  right: 0;
+  top: 21px;
+}
+.login img {
+  border: 1px solid #d9ad50;
+  border-radius: 42px;
+}
+.login span {
+  display: block;
+  float: left;
+}
+.login .log_in {
+  float: left;
+  width: 130px;
+  margin-left: 5px;
+}
+.log_in a {
+  color: #fff;
+}
+.log_in p {
+  font-size: 14px;
+  color: #858792;
+  padding-top: 3px;
+}
+.header_two {
+  width: 100%;
+  position: absolute;
+  top: 86px;
+  left: 0;
+  background: rgba(0, 0, 0, 0.7);
+  transition: all 0.3s ease;
+  z-index: 3;
+  height: 0;
+  overflow: hidden;
+}
+.header_two ul {
+  width: 1200px;
+  height: 240px;
+  margin: 0 auto;
+  padding-top: 15px;
+  padding-bottom: 30px;
+}
+.header_two ul li {
+  float: left;
+  width: 100px;
+  margin-right: 10px;
+}
+.header_two ul li:nth-child(1) {
+  margin-left: 235px;
+}
+.header_two ul li a {
+  display: block;
+  width: 100px;
+  height: 30px;
+  font-size: 14px;
+  color: #c9c9dd;
+  line-height: 30px;
+  text-align: center;
+}
+.header_two ul li a:hover {
+  color: #d9ad50;
+  text-decoration: underline;
+}
+</style>
+
+
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,482 +1,355 @@
 <template>
-    <div class="home">
-        <div class="tou">
-		<ul class="tu">
-			 <li class="oli ali"><img src="../assets/img/tou.jpg" width="220" height="50px"></li>
-			 <li class="oli"><img src="../assets/img/tou1.jpg"/></li>
-			 <li class="ol">
-			 	<h3 class="zi lg">成长守护平台</h3>
-			 	<h3 class="zi">腾讯游戏排行榜</h3>
-			 </li>
-		
-		</ul>
-	</div>
-	<div class="header">
-
-		<!--一级导航-->
-		<div class="header-inner">
-			<div class="log">
-				<img src="../assets/img/alogo.png"/>
-			</div>
-			<ul class="let">
-				<li>
-					<a href="#">官网首页
-					<em>home</em>
-					</a>
-				</li>
-				<li>
-					<a href="#">游戏资料
-					<em>data</em>
-					</a>
-				</li>
-				<li>
-					<a href="#">内容中心
-					<em>contents</em>
-					</a>
-				</li>
-				<li>
-					<a href="#">赛事中心
-					<em>match</em>
-					</a>
-				</li>
-				<li>
-					<a href="#">百态王者
-					<em>culture</em>
-					</a>
-				</li>
-				<li>
-					<a href="#">社区互动
-					<em>community</em>
-					</a>
-				</li>
-				<li>
-					<a href="#">玩家支持
-					<em>player</em>
-					</a>
-				</li>
-			</ul>
-			<div class="login">
-				<span><a href="#"><img src="../assets/img/login.png"/></a></span>
-				<div class="log_in">
-					<a href="#">欢迎登陆</a>
-					<p>登录后查看游戏战绩</p>
-				</div>
-			</div>
-		</div>
-		<!-- 二级导航 -->
-			<div class="header_two">
-				<ul>
-					<li>
-						&nbsp;
-					</li>
-					<li>
-						<a href="#">版本介绍</a>
-						<a href="#">游戏介绍</a>
-						<a href="#">英雄资料</a>
-						<a href="#">爆料站</a>
-						<a href="#">故事站</a>
-						<a href="#">游戏壁纸</a>
-					</li>
-					<li>
-						<a href="#">攻略中心</a>
-						<a href="#">英雄攻略</a>
-						<a href="#">视频中心</a>
-						<a href="#">王者视角</a>
-						<a href="#">作者入驻</a>
-						<a href="#">媒体入驻</a>
-					</li>
-					<li>
-						<a href="#">KPL</a>
-						<a href="#">KPKPL</a>
-						<a href="#">冠军杯</a>
-						<a href="#">高校赛</a>
-						<a href="#">城市赛</a>
-						<a href="#">全民赛</a>
-						<a href="#">赛事数据</a>
-					</li>
-					<li>
-						<a href="#">荣耀·传承</a>
-						<a href="#">王者文化站</a>
-						<a href="#">王者音乐所</a>
-						<a href="#">万千世界</a>
-						<a href="#">星光殿堂</a>
-						<a href="#">WiFi特权</a>
-					</li>
-					<li>
-						<a href="#">王者营地</a>
-						<a href="#">手Q部落</a>
-						<a href="#">微信圈子</a>
-						<a href="#">官方微博</a>
-						<a href="#">微信公众号</a>
-						<a href="#">手Q订阅号</a>
-						<a href="#">同仁社区</a>
-					</li>
-					<li>
-						<a href="#">成长守护平台</a>
-						<a href="#">健康系统</a>
-						<a href="#">狄仁杰护卫站</a>
-						<a href="#">客服专区</a>
-						<a href="#">礼包兑换</a>
-						<a href="#">自助服务</a>
-					</li>
-				</ul>
-
-			</div>
-		
-	</div>
-	<div class="warpper"> 
-		<div class="content">
-		<a href="#" title="查看详情"></a>
-		</div>
-		<div class="main">
-			<div class="main_top">
-				<div class="rotate">
-					<ul :style="{left:num}">
-						<li v-for="(item,index) in list" :key="item.title"><img :src="item.src" ></li>
-					</ul>
-					<div class="box">
-						<span :class="{'active':nowIndex==index}" @mouseover="tab(index)" v-for="(item,index) in list" :key="item.title">{{item.title}}</span>
-					</div>
-				</div>
-				<div class="cont">
-					<div class="cont_a">
-						<ul>
-							<li>热门</li>
-							<li>热门</li>
-							<li>热门</li>
-							<li>热门</li>
-							<li>热门</li>
-							<li>...</li>
-						</ul>
-					</div>
-				</div>
-		</div>
-	</div>
-	</div>
+  <div class="home">
+   <v-header></v-header>
+    <div class="warpper">
+      <div class="content">
+        <a href="#" title="查看详情"></a>
+      </div>
+      <div class="main">
+        <div class="main_top">
+          <div class="rotate">
+            <ul :style="{left:num}">
+              <li v-for="item in list" :key="item.title">
+                <img :src="item.src">
+              </li>
+            </ul>
+            <div class="box">
+              <span
+                :class="{'active':nowIndex==index}"
+                @mouseover="tab(index)"
+                v-for="(item,index) in list"
+                :key="item.title"
+              >{{item.title}}</span>
+            </div>
+          </div>
+          <div class="cont">
+            <div class="cont_a">
+               <div class="dao">
+                 <p :class="{'active_a':nowIndex1==index}" @mouseover="tab2(index)"
+                 v-for="(item,index) in dataList.list" :key="index">{{item.title}}</p>
+                 <p>...</p>
+               </div>
+               <div class="item_a" :style="{left:num1}">
+                  <div class="item" v-for="(item,index) in dataList.list" :key="index">
+                    <div class="cont_c">
+                      <p><a href="#">{{item.name}}</a></p>
+                      <ul>
+                        <li v-for="(option,index) in item.infoList" :key="index">
+                          <span class="biaoti"><a href="#">{{option.text}}</a></span>
+                          <span class="tim">{{option.time}}</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+               </div>
+            </div>
+          </div>
+          <div class="download_pannel">
+              <a href="#" class="download_btn"></a>
+              <a href="#" class="guard_btn"></a>
+              <a href="#" class="tiyan_btn"></a>
+            </div>
+        </div>
+      <div class="quick_entrance">
+          <a href="#"><img src="../assets/img/zhou.jpg" alt=""></a>
+          <a href="#"><img src="../assets/img/zhu.jpg" alt=""></a>
+          <a href="#"><img src="../assets/img/li.jpg" alt=""></a>
+          <a href="#"><img src="../assets/img/kpl.jpg" alt=""></a>
+      </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-export default{
-	data(){
-		return{
-			list:[
-				{
-					title:"夏日全新版本",
-					src:require("../assets/img/01.jpg")
-				},
-				{
-					title:"妮维雅王者洁面",
-					src:require("../assets/img/02.jpg")
-				},
-				{
-					title:"走进稷下学宫",
-					src:require("../assets/img/03.jpg")
-				},
-				{
-					title:"微赛事公会赛",
-					src:require("../assets/img/04.jpg")
-				},
-				{
-					title:"猫神人物志",
-					src:require("../assets/img/05.jpg")
-				}
-			],
-			nowIndex:0,
-			num:0
-		}
-	},
-	methods:{
-		tab(i){
-			this.nowIndex=i;
-			this.num=(this.nowIndex*-604)+"px";
-		}
-		
-	},
-	mounted(){
-			setInterval(()=>{
-				this.nowIndex++;
-				this.nowIndex = this.nowIndex>4?0:this.nowIndex;
-				this.num=(this.nowIndex*-604)+"px";
-			}
-			,3000)
-		}
-}
-
-
+import vHeader from './header.vue'
+export default {
+  components:{
+    vHeader
+  },
+  data() {
+    return {
+      dataList: require("../assets/data/datalist.json"),
+      list: [
+        {
+          title: "夏日全新版本",
+          src: require("../assets/img/01.jpg")
+        },
+        {
+          title: "妮维雅王者洁面",
+          src: require("../assets/img/02.jpg")
+        },
+        {
+          title: "走进稷下学宫",
+          src: require("../assets/img/03.jpg")
+        },
+        {
+          title: "微赛事公会赛",
+          src: require("../assets/img/04.jpg")
+        },
+        {
+          title: "猫神人物志",
+          src: require("../assets/img/05.jpg")
+        }
+      ],
+      nowIndex: 0,
+      num: 0,
+      nowIndex1:0,
+      num1:0
+    };
+  },
+  methods: {
+    tab(i) {
+      this.nowIndex = i;
+      this.num = this.nowIndex * -604 + "px";
+    },
+    tab2(k){
+      this.nowIndex1 = k;
+      this.num1 = this.nowIndex1 * -360 + "px";
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.nowIndex++;
+      this.nowIndex = this.nowIndex > 4 ? 0 : this.nowIndex;
+      this.num = this.nowIndex * -604 + "px";
+    }, 3000);
+  }
+};
 </script>
-<style>
-*{
-	margin: 0;
-	padding: 0;
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
 }
-li{
-	list-style: none;
+li {
+  list-style: none;
 }
-em{
-	font-style: normal;
+em {
+  font-style: normal;
 }
-body{
-	padding-top: 42px;
-	overflow-x: hidden;
+body {
+  padding-top: 42px;
+  overflow-x: hidden;
 }
-a{
-	text-decoration: none;
-	color: #c9c9dd;
+a {
+  text-decoration: none;
+  color: #c9c9dd;
 }
-.tou{
-	width: 100%;
-	border-width: 1px 0;
-	position: absolute;
-	height: 42px;
-	left: 0;
-	top: 0 !important;
-	border: 1px solid #c9c9c9
+.wrapper {
+  position: relative;
 }
-.tou .tu{
-	width: 950px;
-	margin: 0 auto;
+.content {
+  width: 100%;
+  height: 1100px;
+  position: absolute;
+  left: 0;
+  top: 127px;
+  background: url(../assets/img/back.jpg);
+  background-size: 100% 1100px;
 }
-.oli{
-	float: left;
+.content a {
+  width: 199px;
+  height: 60px;
+  position: absolute;
+  top: 458px;
+  left: 690px;
 }
-.ali{
-	width: 205px;
-    padding: 0 15px 0 0;
-    background-position: 0 -40px;
-    position: relative;
-    z-index: 10001
+.main {
+  width: 1200px;
+  padding-top: 590px;
+  margin: 0 auto;
 }
-.ol{
-	float: right;
+.main_top {
+  width: 1200px;
+  height: 342px;
+  position: relative;
+  background: url(../assets/img/rotate_bg.png) no-repeat;
 }
-.zi{
-	float: left;
-	width: 118px;
-	font-size: 12px;
-	font-weight: 400;
-	cursor: pointer;
-	text-indent: 2rem;
-	line-height: 40px;
+.main_top .rotate {
+  width: 604px;
+  height: 342px;
+  overflow: hidden;
+  float: left;
 }
-.lg{
-	background: url(../assets/img/logo.png) 12px no-repeat;
+.main_top .rotate ul {
+  width: 3020px;
+  height: 298px;
+  position: relative;
+  left: 0;
+  top: 0;
+  transition: 0.5s;
 }
-.header{
-	width: 100%;
-	position: absolute;
-	top: 42px;
-	left: 0;
-	height: 85px;
-	background: rgba(0,0,0,0.8);
+.main_top .rotate ul li {
+  width: 604px;
+  height: 298px;
+  float: left;
 }
-.header:hover .header_two{
-	height: 240px;
+.main_top .box {
+  width: 100%;
+  cursor: pointer;
 }
-.log{
-	float: left;
+.main_top .box span {
+  float: left;
+  display: block;
+  width: 120.8px;
+  height: 44px;
+  line-height: 44px;
+  font-size: 14px;
+  text-align: center;
+  color: #b1b2be;
 }
-.log img{
-	position: absolute;
-	top: 16px;
-	left: 0;
+.main_top .box .active {
+  background: rgba(255, 255, 255, 0.15);
+  color: #f3c258;
 }
-.header-inner{
-	width: 1200px;
-	height: 84px;
-	position: relative;
-	margin: 0 auto;
+.cont {
+  width: 360px;
+  float: left;
+  overflow: hidden;
 }
-.let{
-	width: 790px;
-	float: left;
-	padding-left: 230px;
+.cont  .cont_a .dao .active_a{
+  color: #f3c258;
+  color: #f3c258;
+  border-bottom: 3px solid #f3c258;
 }
-.let li{
-	width: 110px;
-	float: left;
-	height: 84px;
-	text-align: center;
+.cont_b {
+  width: 325px;
+  height: 45px;
+  margin: 0 auto;
+  border-bottom: 1px solid #000;
 }
-.let li:hover{
-	border-bottom:3px solid #e4b653;
+.cont_b li {
+  width: 52px;
+  height: 45px;
+  text-align: center;
+  line-height: 45px;
+  color: #b8b9c5;
+  font-size: 14px;
+  float: left;
 }
-.let li:hover a{
-	color:#e4b653;
+.cont .cont_a .item_a{
+  width: 1800px;
+  position: relative;
+  left: 0;
+  transition: .3s ease-in;
 }
-.let li:hover a em{
-	color:#e4b653;
+.cont .cont_a .dao{
+  width: 325px;
+  height: 50px;
+  cursor: pointer;
+  line-height: 50px;
+  margin: 0 27px;
+  border-bottom: 1px solid #000;
 }
-.let li a{
-	display: block;
-	width: 110px;
-	height: 64px;
-	line-height: 30px;
-	color: #c9c9dd;
-	font-size: 18px;
-	padding: 20px 0 0;
+.cont .cont_a .dao p{
+  float: left;
+  width: 52px;
+  height: 46px;
+  font-size: 14px;
+  color: #b8b9c5;
+  text-align: center;
 }
-.let li a em{
-	display: block;
-    padding-top: 5px;
-    font-size: 12px;
-    line-height: 16px;
-    color: #858792;
-    text-transform: uppercase;
+.cont .cont_a .item{
+  width: 360px;
+  float: left;
 }
-.login{
-	width: 182px;
-	position: absolute;
-	right: 0;
-	top: 21px;
+.cont_c ul{
+  width: 325px;
+  margin: 0 auto;
 }
-.login img{
-	border: 1px solid #d9ad50;
-    border-radius: 42px;	
+.cont_c p{
+  width: 325px;
+  height: 36px;
+  background: #414046;
+  text-align: center;
+  line-height: 36px;
+  margin: 20px auto;
+  color: #f3c258;
+  font-size: 18px;
 }
-.login span{
-	display: block;
-	float: left;
+.cont_c p a{
+  color: #f3c258;
+  font-size: 18px;
+  display: block;
+  width: 280px;
+  padding-left: 10px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 }
-.login .log_in{
-	float: left;
-	width: 130px;
-	margin-left: 5px;
+.cont_c p a:hover{
+  text-decoration: underline;
 }
-.log_in a{
-	color: #fff;
+.cont_c li {
+  width: 325px;
+  cursor: pointer;
+  margin-bottom: 10px;
 }
-.log_in p{
-	font-size: 14px;
-	color: #858792;
-	padding-top: 3px;
+.cont_c .biaoti a{
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  color: #b8b9c5;
+  font-size: 14px;
+  float: left;
+  width: 275px;
 }
-.header_two{
-	width: 100%;
-	position: absolute;
-	top:86px;
-	left: 0;
-	background: rgba(0,0,0,0.7);
-	transition: all .3s ease;
-	z-index: 3;
-	height: 0;
-	overflow: hidden;
+.cont_c .biaoti a:hover{
+  text-decoration: underline;
 }
-.header_two ul{
-	width: 1200px;
-	height: 240px;
-	margin: 0 auto;
-	padding-top: 15px;
-	padding-bottom: 30px;
+.cont_c .tim{
+  color: #999;
+  font-size: 12px;
+  margin-left: 16px;
 }
-.header_two ul li{
-	float: left;
-	width: 100px;
-	margin-right: 10px;
+.download_pannel{
+  width: 236px;
+  height: 342px;
+  float: left;
+  overflow: hidden;
+  position: relative;
 }
-.header_two ul li:nth-child(1){
-	margin-left: 235px;
+.download_pannel a{
+    background: url(../assets/img/xuebi.png);
+    display: block;
 }
-.header_two ul li a{
-	display: block;
-	width: 100px;
-	height: 30px;
-	font-size: 14px;
-	color: #c9c9dd;
-	line-height: 30px;
-	text-align: center;
+.download_pannel .download_btn{
+  display: block;
+  width: 236px;
+  height: 128px;
+  background-position: 0 -219px;
 }
-.header_two ul li a:hover{
-	color: #d9ad50;
-	text-decoration: underline;
+.download_pannel .guard_btn{
+  width: 236px;
+  height: 106px;
+  background-position: 0 -350px;
 }
-.wrapper{
-	position: relative;
+.download_pannel .tiyan_btn{
+  width: 236px;
+  height: 108px;
+  background-position: 0 -461px;
 }
-.content{
-	width: 100%;
-	height: 1100px;
-	position: absolute;
-	left: 0;
-	top: 127px;
-	background: url(../assets/img/back.jpg);
-	background-size: 100% 1100px;
+.quick_entrance{
+  width: 1200px;
+  height: 134px;
+  font-size: 0;
+  margin: 28px auto 34px;
 }
-.content a{
-	width: 199px;
-	height: 60px;
-	position: absolute;
-	top: 458px;
-	left: 690px;
+.quick_entrance a{
+  display: inline-block;
+  width: 291px;
+  height: 134px;
+  margin-right: 12px;
+  overflow: hidden;
 }
-.main{
-	width: 1200px;
-	position: relative;
-	padding-top: 590px;
-	margin: 0 auto;
+.quick_entrance a:nth-last-child(1){
+  margin-right: 0;
 }
-.main_top{
-	width: 1200px;
-	height: 342px;
-	background: url(../assets/img/rotate_bg.png);
+.quick_entrance a img{
+  transition: .5s;
 }
-.main_top .rotate{
-	width:604px;
-	height: 342px;
-	overflow: hidden;
-	float: left;
-}
-.main_top .rotate ul{
-	width:3020px;
-	height: 298px;
-	position: relative;
-	left: 0;
-	top: 0;
-	transition: .5s;
-}
-.main_top .rotate ul li{
-	width: 604px;
-	height: 298px;
-	float: left;
-}
-.main_top .box{
-	width: 100%;
-	cursor: pointer;
-}
-.main_top .box span{
-	float: left;
-	display: block;
-	width: 120.8px;
-	height: 44px;
-	line-height:44px;
-	font-size: 14px;
-	text-align: center;
-	color: #b1b2be;
-}
-.main_top .box .active{
-	background: rgba(255,255,255,0.15);
-	color: #f3c258;
-}
-.cont .cont_a{
-	width: 360px;
-	float: left;
-}
-.cont{
-	float: left;
-}
-.cont .cont_a ul{
-	width: 325px;
-	height: 45px;
-	margin: 0 auto;
-	border-bottom: 1px solid #000;
-}
-.cont .cont_a ul li{
-	width: 52px;
-	height: 45px;
-	text-align: center;
-	line-height: 45px;
-	color: #b8b9c5;
-	font-size: 14px;
-	float: left;
+.quick_entrance a img:hover{
+  transform: scale(1.1);
 }
 </style>
 
